6c1b507efd9718c675201639e30e4b2dba292781
TomaTeQuila <amaroeduard@gmail.com>
2024-10-16 15:52:43 -0300

    sda

--- src/fotos/fotos.service.ts
+++ src/fotos/fotos.service.ts
@@ -12,6 +12,11 @@ export class FotosService {
             id: 2,
             url: 'https://media.discordapp.net/attachments/1031376790742122566/1294697382776143934/image.png?ex=670e9766&is=670d45e6&hm=6e706295cc0829351d3ed6cbd86ebeaf4df4c546f69a40a6001547519f796ae1&=&format=webp&quality=lossless&width=243&height=315',
             descripcion: 'Sr. Naranja - 12/10/2024'
+        },
+        {
+            id: 3,
+            url: 'https://media.discordapp.net/attachments/1031376790742122566/1295971442932060262/2024-10-15_23-44-05.png?ex=67113fb5&is=670fee35&hm=f2800f77e7a664be7242fbee2084d5aff2cc79284cc5379f2ce6d0dd68cd95c5&=&format=webp&quality=lossless&width=1193&height=671',
+            descripcion: 'Jhon Hernandez - 16/10/2024'
         }
     ];
 
